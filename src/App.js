90f3595986580e2d3ea180ed0b6183c9007bf1b9
Sakariash <simonsakarias@gmail.com>
2022-04-28 15:23:36 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { data } from "autoprefixer";
 
 function App() {
+  // const hardRandom =
+  //   "https://opentdb.com/api.php?amount=25&difficulty=hard&type=multiple";
+  // fetch(hardRandom)
+  //   .then((response) => response.json())
+  //   .then((data) => console.log(data));
+  // const mediumRandom =
+  //   "https://opentdb.com/api.php?amount=25&difficulty=medium&type=multiple";
+  // fetch(mediumRandom)
+  //   .then((response) => response.json())
+  //   .then((data) => console.log(data));
+  const easyRandom =
+    "https://opentdb.com/api.php?amount=25&difficulty=easy&type=multiple";
+  fetch(easyRandom)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+
+  data.results.map((result) => {
+    console.log(result);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
