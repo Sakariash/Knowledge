9d7c5b035d7e248794d9460940ba2d3d9a7699e9
Sakariash <simonsakarias@gmail.com>
2022-05-03 02:29:45 +0200

    styling

--- src/components/StartComponent/index.js
+++ src/components/StartComponent/index.js
@@ -4,10 +4,13 @@ const Start = ({ props }) => {
   const startQuiz = () => props(true);
 
   return (
-    <div className="pt-48 text-center">
-      <h1 className="text-6xl">Knowledge is power.</h1>
-      <button onClick={startQuiz} className="mt-8">
-        Begin
+    <div className="flex flex-col h-full items-center justify-center">
+      <h1 className="text-6xl font-bold">Knowledge is power</h1>
+      <button
+        onClick={startQuiz}
+        className="border-2 rounded-xl text-[#616a94] bg-[#161a31] select-none cursor-pointer py-4 px-10 hover:bg-[#22242e] mt-8"
+      >
+        Begin Quiz
       </button>
     </div>
   );
