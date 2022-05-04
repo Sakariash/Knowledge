6cbf74c57839ba64aab977b21300d6c91ebd8dd8
Sakariash <simonsakarias@gmail.com>
2022-05-04 07:40:02 +0200

    Update index.js

--- src/components/GameOver/index.js
+++ src/components/GameOver/index.js
@@ -1,12 +1,12 @@
-export default function GameOver({ points, text }) {
+export default function GameOver({ points }) {
   const refreshPage = () => window.location.reload();
 
   return (
     <div className="flex flex-col h-full items-center justify-center text-center">
       <h1 className="text-6xl font-bold mb-1">Game Over</h1>
-      <p className="mb-14">You got {points} points out of 10!</p>
+      <p className="mb-14">You got {points} points out of 15!</p>
       <button
-        className="border-2 rounded-xl text-[#616a94] bg-[#161a31] select-none cursor-pointer py-4 px-10 hover:bg-[#22242e]"
+        className="border-2 rounded-xl border-[#F79673] text-black bg-white select-none cursor-pointer py-4 px-10 hover:bg-[#F79673] hover:text-white font-bold"
         onClick={refreshPage}
       >
         Play Again
