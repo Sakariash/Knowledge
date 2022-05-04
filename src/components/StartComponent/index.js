5d074d039a150313dce93655918cd3a5204e31c8
Sakariash <simonsakarias@gmail.com>
2022-05-04 07:41:21 +0200

    Update index.js

--- src/components/StartComponent/index.js
+++ src/components/StartComponent/index.js
@@ -5,12 +5,12 @@ const Start = ({ props }) => {
 
   return (
     <div className="flex flex-col h-full items-center justify-center">
-      <h1 className="text-6xl font-bold">Knowledge is power</h1>
+      <h1 className="text-3xl md:text-6xl font-bold">Knowledge is power</h1>
       <button
         onClick={startQuiz}
-        className="border-2 rounded-xl text-[#616a94] bg-[#161a31] select-none cursor-pointer py-4 px-10 hover:bg-[#22242e] mt-8"
+        className="border-4 rounded-xl border-[#F79673] text-black bg-white  select-none cursor-pointer py-4 px-10 hover:bg-[#F79673] hover:text-white mt-8 font-bold"
       >
-        Begin Quiz
+        Start Quiz
       </button>
     </div>
   );
