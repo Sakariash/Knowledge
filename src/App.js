c426b6b663a1a0146b54ad127e4c709e1eeb6fbb
Sakariash <simonsakarias@gmail.com>
2022-05-04 07:39:39 +0200

    styling

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ export default function App() {
   const [start, setStart] = useState(false);
 
   return (
-    <div className="quiz w-screen h-screen bg-[#101427] text-white">
+    <div
+      className="quiz w-screen h-screen bg-[rgb(2,0,36)]
+      bg-[linear-gradient(45deg, rgba(2,0,36,1) 0%, rgba(89,110,235,1) 0%, rgba(51,149,242,1) 33%)]"
+    >
       {start ? <Quiz /> : <Start props={setStart} />}
     </div>
   );
